a77ecf2ca0e5e77011dd619572654c99997bb036
Edward Hinkle <eddie@meetmaestro.com>
2016-03-01 21:35:12 -0500

    Add ngGAPI module to angularjs
    #3

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('annualTimeBlock', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr']);
+    .module('annualTimeBlock', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr', 'ngGAPI']);
 
 })();

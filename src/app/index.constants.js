eafc3c99c701388b4db6f7cd9bd6cdd9f127f4b0
Edward Hinkle <eddie@meetmaestro.com>
2016-03-01 21:25:16 -0500

    Initial yeoman generation of angularjs app
    Close #1

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('annualTimeBlock')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('annualTimeBlock', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();

4bcb53954b7599ef3cf654f78fe3bf19caedd25a
Edward Hinkle <eddie@meetmaestro.com>
2016-03-03 22:21:46 -0500

    Replace ngGAPI settings with angular-googleapi
    settings. #6

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -3,12 +3,11 @@
 
   angular
     .module('annualTimeBlock')
-    .value('GoogleApp', {
-      apiKey: 'AIzaSyCcgg8cp_fEA3nXRfTtM1E6wRwKkSsMLio',
-      clientId: '792481441566-uq4rseenlu6pifbnojdgktqfng7lg6fv.apps.googleusercontent.com',
-      scopes: [
-        'https://www.googleapis.com/auth/calendar'
-      ]
+    .config(function(googleLoginProvider) {
+        googleLoginProvider.configure({
+            clientId: '792481441566-uq4rseenlu6pifbnojdgktqfng7lg6fv.apps.googleusercontent.com',
+            scopes: ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/plus.login"]
+        });
     })
     .run(runBlock);
 

5535c8817eeb2e1bfeb8f6a7cfb389a928f4f50c
Edward Hinkle <eddie@meetmaestro.com>
2016-03-03 20:18:40 -0500

    Add Google APIs key to connect to Google services
    #3

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -3,12 +3,19 @@
 
   angular
     .module('annualTimeBlock')
+    .value('GoogleApp', {
+      apiKey: 'AIzaSyCcgg8cp_fEA3nXRfTtM1E6wRwKkSsMLio',
+      clientId: '792481441566-uq4rseenlu6pifbnojdgktqfng7lg6fv.apps.googleusercontent.com',
+      scopes: [
+        'https://www.googleapis.com/auth/calendar'
+      ]
+    })
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock() {
 
-    $log.debug('runBlock end');
+
   }
 
 })();

a5356bd524fc9662b2de3486a12aeb8d963b5345
Edward Hinkle <eddie@meetmaestro.com>
2016-03-03 20:53:37 -0500

    replace ngGAPI with angular-googleapi
    #6

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('annualTimeBlock', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr', 'gapi']);
+    .module('annualTimeBlock', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr', 'googleApi']);
 
 })();
